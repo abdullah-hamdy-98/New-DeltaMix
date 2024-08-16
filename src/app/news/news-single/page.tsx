310fb8b3fe8d0450d59d5468bbb384cec92e30b3
abdullah-hamdy-98 <abdo1231992260@gmail.com>
2024-08-16 17:37:09 +0300

    15

--- src/app/news/news-single/page.tsx
+++ src/app/news/news-single/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import Loader from '../../components/Loader';
 import Mobile from '../../components/MobileVersion'
 import Navbar from '../../components/Navbar';
